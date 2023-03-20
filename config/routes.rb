2a4b2db358a8b39a05b664f84d92fe3ed38282bf
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-03-20 05:28:37 +0000

    dbmigrate user todo

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,42 @@ Rails.application.routes.draw do
 
 
 
+  # Routes for the User resource:
+
+  # CREATE
+  post("/insert_user", { :controller => "users", :action => "create" })
+          
+  # READ
+  get("/users", { :controller => "users", :action => "index" })
+  
+  get("/users/:path_id", { :controller => "users", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_user/:path_id", { :controller => "users", :action => "update" })
+  
+  # DELETE
+  get("/delete_user/:path_id", { :controller => "users", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Todo resource:
+
+  # CREATE
+  post("/insert_todo", { :controller => "todos", :action => "create" })
+          
+  # READ
+  get("/todos", { :controller => "todos", :action => "index" })
+  
+  get("/todos/:path_id", { :controller => "todos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_todo/:path_id", { :controller => "todos", :action => "update" })
+  
+  # DELETE
+  get("/delete_todo/:path_id", { :controller => "todos", :action => "destroy" })
+
+  #------------------------------
+
 end
